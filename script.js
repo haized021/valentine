e70237be801efc48e30f8b58bf5a4ee285d3fb68
haized021 <haized021@gmail.com>
2026-02-04 20:56:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const card = document.getElementById("card");
 
-function moveNoButton() {
-  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
-  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-
-  noBtn.style.left = `${x}px`;
-  noBtn.style.top = `${y}px`;
+// Function to move the NO button
+function moveButton() {
+  const x = Math.random() * 240 - 120;
+  const y = Math.random() * 240 - 120;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
 }
 
-// Move on hover (PC)
-noBtn.addEventListener("mouseover", moveNoButton);
+// NO button runs away when CLICKED
+noBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  moveButton();
+});
 
-// Move on touch (Mobile)
-noBtn.addEventListener("touchstart", moveNoButton);
-
-// YES button effect
+// YES button
 yesBtn.addEventListener("click", () => {
-  document.body.innerHTML = `
-    <div style="text-align:center">
-      <h1>YAY!! 💖🥰</h1>
-      <p>I knew it 😘</p>
-      <img src="https://media.tenor.com/8GZNOvQ4pPAAAAAC/love-bear.gif" width="250">
-    </div>
+  card.innerHTML = `
+    <h1>YAY!!! 💖🥰</h1>
+    <p>I knew you’d say yes 😭💘</p>
+    <img src="yes.png" class="cute-img">
+    <p>You just made my day 💕</p>
   `;
 });
+
+
